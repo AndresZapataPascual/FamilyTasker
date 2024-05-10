e685b577fd0c0c49cfd37777662ad8370ce41399
AndresZapataPascual <andres.zapata019@pascualbravo.edu.co>
2024-05-10 10:19:42 -0500

    integracion animacion
    1

--- app/src/main/java/com/andreszapata/familytasker/Pantalla_splash.java
+++ app/src/main/java/com/andreszapata/familytasker/Pantalla_splash.java
@@ -3,8 +3,8 @@ package com.andreszapata.familytasker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
 import androidx.appcompat.app.AppCompatActivity;
+import com.airbnb.lottie.LottieAnimationView;
 
 public class Pantalla_splash extends AppCompatActivity {
 
@@ -14,12 +14,19 @@ public class Pantalla_splash extends AppCompatActivity {
         setContentView(R.layout.splash_layout);
 
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startActivity(new Intent(Pantalla_splash.this,MainActivity.class));
-                finish();
-            }
-        }, 5000);
+        LottieAnimationView animationView = findViewById(R.id.animationView);
+        animationView.setAnimation(R.raw.animation);
+        animationView.playAnimation();
+
+
+        new Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        startActivity(new Intent(Pantalla_splash.this, MainActivity.class));
+                        finish();
+                    }
+                },
+                5000
+        );
     }
 }
